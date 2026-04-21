aa60123419a98ea142104116f24405ad9d2f937a
Kondreddy Balaji Sudarshan Reddy <kondreddy2007@gmail.com>
2026-04-21 21:47:27 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ require('dotenv').config()
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173","https://blogchain-pearl.vercel.app"],
   credentials: true
 }))
 
